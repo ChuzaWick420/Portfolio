87196f1e5db6fb72c7452ae32980a38fb286b930
ChuzaWick420 <zaiddadev@gmail.com>
2025-04-15 18:28:47 +0500

    Added: username

--- src/script.js
+++ src/script.js
@@ -4,8 +4,29 @@ async function init() {
 
     updateImage(jsonData["avatar_url"]);
 
+    updateName(jsonData["name"], jsonData["login"]);
+
     //debug
     console.log(jsonData);
+}
+
+function updateName(name, githubName) {
+    let contacts = document.getElementById("contact_container");
+
+    let text = document.createElement("p");
+
+    let real_name = document.createTextNode(name);
+    let anonymous_name = document.createTextNode(githubName);
+
+    let separator = document.createElement("span");
+    separator.id = "name_separator";
+    separator.appendChild(document.createTextNode("@"));
+
+    text.appendChild(real_name);
+    text.appendChild(separator);
+    text.appendChild(anonymous_name);
+
+    contacts.appendChild(text);
 }
 
 function updateImage(image_url) {
