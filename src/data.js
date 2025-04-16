dfc3198c02b5800579e59c6d4f9206296c2bb39f
ChuzaWick420 <zaiddadev@gmail.com>
2025-04-16 15:09:02 +0500

    Transformed json to js

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const data = {
+    Socials: {
+        "github": {
+            placeholder: "github.com/ChuzaWick420",
+            url: "https://github.com/ChuzaWick420"
+        },
+        "itch.io": {
+            placeholder: "itch.io/profile/chuzawick420",
+            url: "https://itch.io/profile/chuzawick420"
+        },
+        "ShaderToy": {
+            placeholder: "shadertoy.com/profile/ChuzaWick420",
+            url: "https://www.shadertoy.com/profile/ChuzaWick420"
+        },
+        "NexusMode": {
+            placeholder: "next.nexusmods.com/profile/ChuzaWick420",
+            url: "https://next.nexusmods.com/profile/ChuzaWick420"
+        },
+        "ArtStation": {
+            placeholder: "artstation.com/chuzawick420",
+            url: "https://www.artstation.com/chuzawick420"
+        },
+        "CurseForge": {
+            placeholder: "curseforge.com/members/chuzawick420/projects",
+            url: "https://www.curseforge.com/members/chuzawick420/"
+        },
+        "Modrinth": {
+            placeholder: "modrinth.com/user/ChuzaWick420",
+            url: "https://modrinth.com/user/ChuzaWick420"
+        },
+        "ShaderLabs": {
+            placeholder: "shaderlabs.org/wiki/User:492543441826349066",
+            url: "https://shaderlabs.org/wiki/User:492543441826349066"
+        },
+        "GitLab": {
+            placeholder: "gitlab.com/ChuzaWick420",
+            url: "https://gitlab.com/ChuzaWick420"
+        },
+    },
+    Projects: {
+        "notes_publisher": {
+            comments: [
+                "Markdown notes",
+                "notes website",
+                "Uses LaTeX, python, yml, js, css, sh"
+            ],
+            code: {
+                placeholder: "github.com/ChuzaWick420/notes_publisher",
+                url: "https://github.com/ChuzaWick420/notes_publisher"
+            },
+            source: "https://chuzawick420.github.io/notes_publisher/"
+        },
+        "Ray Tracing": {
+            comments: [
+                "Multi-threaded",
+                "Photorealistic"
+            ],
+            code: {
+                placeholder: "github.com/ChuzaWick420/rayTracing",
+                url: "https://github.com/ChuzaWick420/rayTracing"
+            },
+            source: "https://chuzawick420.github.io/notes_publisher/Projects/rayTracing/proj_raytracing_main/"
+        }
+    },
+    Experience: {},
+    Contributions: {}
+}
--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import { data } from "./data.js";
+
 async function init() {
     let githubProfileData = await fetch("https://api.github.com/user/114288630");
     let jsonData = await githubProfileData.json();
@@ -8,6 +10,7 @@ async function init() {
 
     //debug
     console.log(jsonData);
+    updatePage();
 }
 
 function updateName(name, githubName) {
@@ -31,6 +34,42 @@ function updateName(name, githubName) {
 
 function updateImage(image_url) {
     document.getElementById("pfp").src = image_url;
+}
+
+function updatePage() {
+
+    let isFirst = true;
+
+    const sections_container = document.getElementById("sections_container");
+
+    for (const [key, value] of Object.entries(data)) {
+        if (isFirst) {
+            isFirst = false;
+            continue;
+        }
+
+        const separator = document.createElement("hr");
+        sections_container.appendChild(separator);
+
+        // TODO: BLUEPRINT
+        // DIV
+        //      Heading
+        //      UL
+        //          Heading
+        //          UL
+        //              content
+
+        const section_div = document.createElement("div");
+
+        const section_heading = document.createElement("h1");
+        section_heading.innerText = key;
+
+        section_div.appendChild(section_heading);
+
+        sections_container.appendChild(section_div);
+        console.log(key, value);
+
+    }
 }
 
 init();
