e92aebef96c4ba0f111c6a8eb3fa308a52530148
ChuzaWick420 <zaiddadev@gmail.com>
2025-04-20 15:30:57 +0500

    Updated: portfolio

--- src/data.js
+++ src/data.js
@@ -8,45 +8,56 @@ export const data = {
             placeholder: "linkedin.com/in/chuzawick420/",
             url: "https://www.linkedin.com/in/chuzawick420/"
         },
-        "Itch.io": {
-            placeholder: "itch.io/profile/chuzawick420",
-            url: "https://itch.io/profile/chuzawick420"
-        },
-        "ShaderToy": {
-            placeholder: "shadertoy.com/profile/ChuzaWick420",
-            url: "https://www.shadertoy.com/profile/ChuzaWick420"
-        },
-        "NexusMode": {
-            placeholder: "next.nexusmods.com/profile/ChuzaWick420",
-            url: "https://next.nexusmods.com/profile/ChuzaWick420"
-        },
-        "ArtStation": {
-            placeholder: "artstation.com/chuzawick420",
-            url: "https://www.artstation.com/chuzawick420"
-        },
-        "CurseForge": {
-            placeholder: "curseforge.com/members/chuzawick420/projects",
-            url: "https://www.curseforge.com/members/chuzawick420/"
-        },
-        "Modrinth": {
-            placeholder: "modrinth.com/user/ChuzaWick420",
-            url: "https://modrinth.com/user/ChuzaWick420"
-        },
-        "ShaderLabs": {
-            placeholder: "shaderlabs.org/wiki/User:492543441826349066",
-            url: "https://shaderlabs.org/wiki/User:492543441826349066"
-        },
-        "Gitlab": {
-            placeholder: "gitlab.com/ChuzaWick420",
-            url: "https://gitlab.com/ChuzaWick420"
+        //"Itch.io": {
+        //    placeholder: "itch.io/profile/chuzawick420",
+        //    url: "https://itch.io/profile/chuzawick420"
+        //},
+        //"ShaderToy": {
+        //    placeholder: "shadertoy.com/profile/ChuzaWick420",
+        //    url: "https://www.shadertoy.com/profile/ChuzaWick420"
+        //},
+        //"NexusMode": {
+        //    placeholder: "next.nexusmods.com/profile/ChuzaWick420",
+        //    url: "https://next.nexusmods.com/profile/ChuzaWick420"
+        //},
+        //"ArtStation": {
+        //    placeholder: "artstation.com/chuzawick420",
+        //    url: "https://www.artstation.com/chuzawick420"
+        //},
+        //"CurseForge": {
+        //    placeholder: "curseforge.com/members/chuzawick420/projects",
+        //    url: "https://www.curseforge.com/members/chuzawick420/"
+        //},
+        //"Modrinth": {
+        //    placeholder: "modrinth.com/user/ChuzaWick420",
+        //    url: "https://modrinth.com/user/ChuzaWick420"
+        //},
+        //"ShaderLabs": {
+        //    placeholder: "shaderlabs.org/wiki/User:492543441826349066",
+        //    url: "https://shaderlabs.org/wiki/User:492543441826349066"
+        //},
+        //"Gitlab": {
+        //    placeholder: "gitlab.com/ChuzaWick420",
+        //    url: "https://gitlab.com/ChuzaWick420"
+        //},
+    },
+    Experience: {
+        "Inbound Agency LTD": {
+            comments: [
+                "Front End Intern",
+                "Learnt the basis of HTML, CSS, JavaScript, React",
+                "Learnt handling Data Base queries (Strapi) from client",
+                "Worked in Multan, Pakistan",
+                "Worked between Jan 2024 - Feb 2024",
+            ],
         },
     },
     Projects: {
         "notes_publisher": {
             comments: [
                 "Markdown notes",
-                "notes website",
-                "Uses LaTeX, python, yml, js, css, sh"
+                "A website for my notes",
+                "Uses LaTeX, Python, YAML, JavaScript, CSS, sh"
             ],
             code: {
                 placeholder: "github.com/ChuzaWick420/notes_publisher",
@@ -76,8 +87,14 @@ export const data = {
             },
         },
     },
-    Experience: {},
-    Contributions: {},
-    Education: {},
-    Interests: {}
+    Education: {
+        "Virtual University of Pakistan": {
+            comments: [
+                "Enrolled in BS - Computer Science",
+                "Study year: 2022 - 2026"
+            ],
+        }
+    },
+    //Contributions: {},
+    //Interests: {}
 }
